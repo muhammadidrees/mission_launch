8664460f0571e90b51a35294c4781bd8831d12af
Muhammad Idrees <midrees097@gmail.com>
2025-04-23 22:04:45 +0500

    refactor spaceship size and animation loading in onLoad method

--- lib/game/entities/spaceship/spaceship.dart
+++ lib/game/entities/spaceship/spaceship.dart
@@ -3,6 +3,7 @@ import 'package:flame/components.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:flutter/material.dart';
 import 'package:mission_launch/game/entities/spaceship/behaviors/behaviors.dart';
+import 'package:mission_launch/gen/assets.gen.dart';
 
 /// {@template spaceship}
 /// A spaceship that can be controlled with left/right arrow keys.
@@ -14,7 +15,8 @@ class Spaceship extends PositionedEntity with HasGameReference {
     this.maxHealth = 3,
   }) : super(
           anchor: Anchor.center,
-          size: Vector2(48, 32),
+          size: Vector2(40, 56),
+          scale: Vector2.all(2),
           behaviors: [
             KeyboardMovingBehavior(),
             PropagatingCollisionBehavior(
@@ -64,23 +66,18 @@ class Spaceship extends PositionedEntity with HasGameReference {
 
   @override
   Future<void> onLoad() async {
-    await add(
-      RectangleComponent(
-        size: size,
-        paint: Paint()
-          ..color = Colors.white
-          ..style = PaintingStyle.fill,
+    final animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache(Assets.images.spaceshipIdle.path),
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        stepTime: 0.1,
+        textureSize: Vector2(40, 56),
       ),
     );
-
-    // Add a small cockpit to make it look like a spaceship
     await add(
-      RectangleComponent(
-        size: Vector2(16, 10),
-        position: Vector2(16, 0),
-        paint: Paint()
-          ..color = Colors.blue
-          ..style = PaintingStyle.fill,
+      SpriteAnimationComponent(
+        animation: animation,
+        size: size,
       ),
     );
   }
